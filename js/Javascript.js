f9fef3c4617b8461371a69561576b74144ee11e5
UmutoniSandrine <umutonisandrine250@gmail.com>
2020-09-26 14:27:34 +0200

    js commit5

--- js/Javascript.js
+++ js/Javascript.js
@@ -5,9 +5,18 @@ var namesOfMales = ["Adwoa", "Abenaa", "Akua", " Yaa", "Afua", "Ama", "Akosua"];
 
 function validate() {
     var genders = document.getElementsByName("gender");
-    if (document.form.year.value == "" || document.form.year.value.length != 4 || document.form.year.value > 2021 || document.form.year.value > 1940) {
+    if (document.form.year.value == "" || document.form.year.value.length != 4 || document.form.year.value > 2021
+        || document.form.year.value > 1940) {
         alert("please put year of birth");
         document.form.year.focus();
         return false;
     }
+    else if (document.form.month.value == "" || isNaN(document.form.month.value) || document.form.month.value.length != 2
+        || document.form.month.value > 12 || document.form.month.value <= 0) {
+        alert("Month of birth must be between 1 and 12")
+        document.form.month.focus();
+        return false;
+
+    }
+
 }
