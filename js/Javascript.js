7f9ee1a785be6ce888e2011018b40ec19b255d6a
UmutoniSandrine <umutonisandrine250@gmail.com>
2020-09-26 14:07:58 +0200

    js commit3

--- js/Javascript.js
+++ js/Javascript.js
@@ -1,5 +1,13 @@
-var CC,YY,MM,DD,weekDays,dayVal;
-var namesOfDays=["Monday","Tuesday","Wednesday","Thursday","Friday","Saturday","Sunday"];
-var namesOfFemales=["Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame","Kwasi"];
-var namesOfMales=["Adwoa", "Abenaa", "Akua", " Yaa", "Afua", "Ama","Akosua"];
+var CC, YY, MM, DD, weekDays, dayVal;
+var namesOfDays = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"];
+var namesOfFemales = ["Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame", "Kwasi"];
+var namesOfMales = ["Adwoa", "Abenaa", "Akua", " Yaa", "Afua", "Ama", "Akosua"];
 
+function validate() {
+    var genders = document.getElementsByName("gender");
+    if (document.form.year.value == "" || document.form.year.value.length != 4 || document.form.year.value > 2021 || document.form.year.value > 1940) {
+        alert("please put year of birth");
+        document.form.year.focus();
+        return false;
+    }
+}
