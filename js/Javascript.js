bdcc65186c21d2cfd59949ec42957a74f4d0976b
UmutoniSandrine <umutonisandrine250@gmail.com>
2020-09-26 13:55:08 +0200

    js commint1

---
+++ js/Javascript.js
@@ -0,0 +1,5 @@
+var CC,YY,MM,DD,weekDays,dayVal;
+var namesOfDays=["Monday","Tuesday","Wednesday","Thursday","Friday","Saturday","Sunday"];
+var namesOfFemales=["Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame","Kwasi"];
+var namesOfMales=["Adwoa", "Abenaa", "Akua", " Yaa", "Afua", "Ama","Akosua"];
+
