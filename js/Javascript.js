d1e0bc029efe383c4f10c34a60d8377f001dd811
UmutoniSandrine <umutonisandrine250@gmail.com>
2020-09-26 17:23:53 +0200

    js commit27

--- js/Javascript.js
+++ js/Javascript.js
@@ -5,7 +5,7 @@ var femaleNames = ["Akosua", "Adwoa", "Abenaa", "Akua", " Yaa", "Afua", "Ama"];
 
 function validate() {
     var genders = document.getElementsByName("gender");
-    if (document.form.year.value == "" || document.form.year.value.length != 4 || document.form.year.value > 2021|| document.form.year.value <= 1940) {
+    if (document.form.year.value == "" || document.form.year.value.length != 4 || document.form.year.value > 2021 || document.form.year.value <= 1940) {
         alert("Please Enter The Year of Birth");
         document.form.year.focus();
         return false;
@@ -32,85 +32,79 @@ function validate() {
 
 }
 
-function findDayvalue() {
+function findDayValue() {
     year = document.getElementById("year").value;
-    CC = parseInt(year.subString(0, 2));
-    YY = parseInt(year.subString(2, 4));
+    CC = parseInt(year.substring(0, 2));
+    YY = parseInt(year.substring(2, 4));
     MM = parseInt(document.getElementById("month").value);
     DD = parseInt(document.getElementById("date").value);
-    d = (((CC / 4) - 2 * CC - 1) + ((5 * YY / 4)) + ((26 * (MM + 1) / 10)) + DD) % 7;
+    d = (((CC / 4) - 2 * CC - 1) + ((5 * YY / 4)) + ((26 * (MM + 1) / 10)) + DD) % 7; z
     console.log(d);
     return (Math.floor(d));
 }
+
 function byGender() {
     var genders = document.getElementsByName("gender");
-    if (gender[0].checked == true) {
+    if (genders[0].checked == true) {
         var gender = "male";
-    } else if (genders[1].checked == true) {
+    }
+    else if (genders[1].checked == true) {
         var gender = "female";
-    } else {
+    }
+    else {
         return false;
-
     }
     switch (gender) {
         case "male":
             if (dayValue == 1) {
-                alert("Born on : " + dayNames[0] + " Goes with this Akan name: " + maleNames[0]);
+                alert("Born on " + dayNames[0] + " Goes with this akan name:" + maleNames[0]);
             }
             else if (dayValue == 2) {
-                alert("Born on : " + dayNames[1] + " Goes with this Akan name: " + maleNames[1]);
-            } else if (dayValue == 3) {
-                alert("Born on : " + dayNames[2] + " Goes with this Akan name: " + maleNames[2]);
-
+                alert("Born on " + dayNames[1] + " Goes with this akan name: " + maleNames[1]);
             }
-            if (dayValue == 4) {
-                alert("Born on : " + dayNames[3] + " Goes with this Akan name: " + maleNames[3]);
-            }
-            else if (dayValue == 5) {
-                alert("Born on : " + dayNames[4] + " Goes with this Akan name: " + maleNames[4]);
-            } else if (dayValue == 6) {
-                alert("Born on : " + dayNames[5] + " Goes with this Akan name: " + maleNames[5]);
-
-            }
-            else if (dayValue == -0) {
-                alert("Born on : " + dayNames[5] + " Goes with this Akan name: " + maleNames[6]);
-                break;
-
-            }
-        case "female":
-            if (dayValue == 1) {
-                alert("Born on : " + dayNames[0] + " Goes with this Akan name: " + femaleNames[0]);
-            }
-            else if (dayValue == 2) {
-                alert("Born on : " + dayNames[1] + " Goes with this Akan name: " + femaleNames[1]);
-
-            }
-            else if (dayvalue == 3) {
-                alert("Born on : " + dayNames[2] + " Goes with this Akan name: " + femaleNames[2]);
+            else if (dayValue == 3) {
+                alert("Born on " + dayNames[2] + " Goes with this akan name: " + maleNames[2]);
             }
             else if (dayValue == 4) {
-                alert("Born on : " + dayNames[3] + " Goes with this Akan name: " + femaleNames[3]);
-
+                alert("Born on  " + dayNames[3] + " Goes with this akan name: " + maleNames[3]);
             }
             else if (dayValue == 5) {
-                alert("Born on : " + dayNames[4] + " Goes with this Akan name: " + femaleNames[4]);
+                alert("Born on  " + dayNames[4] + " Goes with this akan name: " + maleNames[4]);
             }
             else if (dayValue == 6) {
-                alert("Born on : " + dayNames[5] + " Goes with this Akan name: " + femaleNames[5]);
-
+                alert("Born on " + dayNames[5] + " Goes with this akan name: " + maleNames[5]);
             }
             else if (dayValue == -0) {
-                alert("Born on : " + dayNames[6] + " Goes with this Akan name: " + femaleNames[6]);
+                alert("Born on " + dayNames[6] + " Goes with this akan name:" + maleNames[6]);
             }
             break;
+        case "female":
+            if (dayValue == 1) {
+                alert("Born on  " + dayNames[0] + " Goes with this akan name: " + femaleNames[0]);
+            }
+            else if (dayValue == 2) {
+                alert("Born on " + dayNames[1] + " Goes with this akan name: " + femaleNames[1]);
+            }
+            else if (dayValue == 3) {
+                alert("Born on " + dayNames[2] + " Goes with this akan name:" + femaleNames[2]);
+            }
+            else if (dayValue == 4) {
+                alert("Born on " + dayNames[3] + " Goes with this akan name: " + femaleNames[3]);
+            }
+            else if (dayValue == 5) {
+                alert("Born on " + dayNames[4] + " Goes with this akan name: " + femaleNames[4]);
+            }
+            else if (dayValue == 6) {
+                alert("Born on " + dayNames[5] + " Goes with this akan name: " + femaleNames[5]);
+            } else if (dayValue == -0) {
+                alert("Born on " + dayNames[6] + " Goes with this akan name: " + femaleNames[6]);
+            }
+            break
         default:
 
-
-
     }
-
 }
 function getName() {
-    dayVal = findDayvalue();
+    dayValue = findDayValue();
     byGender();
 }
