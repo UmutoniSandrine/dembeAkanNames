a8fe84d476066e837aa55198c0a32edb776f41b7
UmutoniSandrine <umutonisandrine250@gmail.com>
2020-09-26 15:44:37 +0200

    js commit21

--- js/Javascript.js
+++ js/Javascript.js
@@ -5,17 +5,16 @@ var namesOfMales = ["Adwoa", "Abenaa", "Akua", " Yaa", "Afua", "Ama", "Akosua"];
 
 function validate() {
     var genders = document.getElementsByName("gender");
-    if (document.form.year.value == "" || document.form.year.value.length != 4 || document.form.year.value > 2021|| document.form.year.value <= 1940) {
+    if (document.form.year.value == "" || document.form.year.value.length != 4 || document.form.year.value > 2021 || document.form.year.value <= 1940) {
         alert("Please Enter The Year of Birth");
         document.form.year.focus();
         return false;
     }
-    else if (document.form.month.value == "" || isNaN(document.form.month.value) || document.form.month.value.length != 2
-        || document.form.month.value > 12 || document.form.month.value <= 0) {
-        alert("Month of birth must be between 1 and 12")
+    else if (document.form.month.value == "" || isNaN(document.form.month.value) ||
+        document.form.month.value.length != 2 || document.form.month.value > 12 || document.form.month.value <= 0) {
+        alert("Please Month of birth must be in 1 and 12");
         document.form.month.focus();
         return false;
-
     }
     else if (document.form.date.value == "" || isNaN(document.form.month.value) ||
         document.form.month.value.length != 2 || document.form.date.value > 31 || document.form.date.value <= 0) {
